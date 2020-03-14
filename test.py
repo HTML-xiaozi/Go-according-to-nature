8e2df9602780db8e65d3d21ca7b800f8eff4144d
HTML-xiaozi <59171010+HTML-xiaozi@users.noreply.github.com>
2020-03-14 13:13:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,14 +4,14 @@ import os
 import time
 
 """请求网页"""
-respones = requests.get('https://www.vmgirls.com/12945.html')
+respones = requests.get('URL')
 html = respones.text
 """解析网页"""
 dir_name = re.findall('<h1 class="post-title h3">(.*?)</h1>',html)[-1]
 if not os.path.exists(dir_name):
     os.mkdir(dir_name)
 
-urls = re.findall('<a href="(.*?)" alt="你与晚霞同样浪漫" title="你与晚霞同样浪漫">',html)
+urls = re.findall('<a href="(.*?)" alt="" title="">',html)
 print(urls)
 
 
